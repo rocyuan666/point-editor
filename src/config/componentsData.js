4522015369f77643636b7fdfdc87ffa46cc13dc1
袁鹏 <rocyuan666@163.com>
2023-07-17 10:58:32 +0800

    fix

--- src/config/componentsData.js
+++ src/config/componentsData.js
@@ -3,9 +3,6 @@ export default [
     tag: 'p',
     name: '文本',
     value: '文本文本',
-    data: {
-      code: '',
-    },
     style: {
       fontSize: '60px',
       color: '#fff',
@@ -13,21 +10,24 @@ export default [
       top: 0,
       zIndex: 'auto',
     },
+    data: {
+      param: '',
+    },
   },
   {
     tag: 'img',
     name: '图片',
     src: 'icon-cam.png',
     alt: '图片',
-    data: {
-      code: '',
-    },
     style: {
       width: '254px',
       height: '254px',
       left: 0,
       top: 0,
       zIndex: 'auto',
+    },
+    data: {
+      param: '',
     },
   },
 ]
